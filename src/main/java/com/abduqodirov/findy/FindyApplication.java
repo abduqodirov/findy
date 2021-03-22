7e45757e819ac06a1003a7c93803840193e7430e
Abdulaziz <abdulazizabduqodirov13@gmail.com>
2021-03-22 13:16:04 +0500

    Initial commit

---
+++ src/main/java/com/abduqodirov/findy/FindyApplication.java
@@ -0,0 +1,13 @@
+package com.abduqodirov.findy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abduqodirov/findy/FindyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abduqodirov.findy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
