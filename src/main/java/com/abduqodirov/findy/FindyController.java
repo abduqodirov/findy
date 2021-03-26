eebabab79943c28c60b82d3d115c02ff5d7c2bbb
Abdulaziz <abdulazizabduqodirov13@gmail.com>
2021-03-26 16:46:58 +0500

    Sign in sign up implemented

--- src/main/java/com/abduqodirov/findy/FindyController.java
+++ src/main/java/com/abduqodirov/findy/FindyController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +11,8 @@ import java.util.List;
 public class FindyController {
 
     ArrayList<LostItem> lostItems = new ArrayList<>();
+    ArrayList<User> users = new ArrayList<>();
+
 
     @GetMapping("/getlostitems")
     public List<LostItem> getLostItems() {
@@ -31,5 +32,51 @@ public class FindyController {
 
         return new Response(200, "Successfully added");
     }
+
+    @PostMapping("/signup")
+    public Response signUp(
+            @RequestParam(value = "firstName", required = true) String firstName,
+            @RequestParam(value = "lastName", required = true) String lastName,
+            @RequestParam(value = "email", required = true) String email,
+            @RequestParam(value = "phoneNumber", required = true) String phoneNumber,
+            @RequestParam(value = "password", required = true) String password
+    ) {
+
+
+        User user = new User(
+                firstName,
+                lastName,
+                email,
+                phoneNumber,
+                password
+        );
+
+        users.add(user);
+
+        return new Response(200, "Successfully signed up");
+    }
+
+    @GetMapping("/signin")
+    public Response signIn(
+            @RequestParam(value = "email", required = true) String email,
+            @RequestParam(value = "password", required = true) String password
+    ) {
+
+
+        for (User user : users) {
+
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+
+                return new Response(200, "You've succesffully signed in");
+
+            }
+
+
+        }
+
+        return new Response(200, "Password or email incorrect");
+    }
+
+
 
 }
